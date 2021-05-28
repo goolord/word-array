b047d0c6d29ef15da70375f998e41a81d32a1292
Michael Peyton Jones <me@michaelpj.com>
2021-05-28 14:45:11 +0100

    Work on benchmarks

--- bench/Main.hs
+++ bench/Main.hs
@@ -14,29 +14,49 @@
     -fno-warn-orphans
 #-}
 
-import Test.Tasty.Bench (defaultMain, bench, nf)
+import Test.Tasty.Bench (bench, bgroup, defaultMain, env, nf, nfIO)
 
-import Data.Word
 import Control.DeepSeq
-import Unsafe.Coerce
+import Control.Monad.Primitive
 import Control.Exception
-import qualified Weigh
+  
+import Data.Word
 import Data.Monoid
 import Data.Word64Array.Word8
 import Data.Functor.Const
+import Data.Foldable (for_)
+import Data.Primitive
+
+import Unsafe.Coerce
 
 deriving instance NFData (WordArray)
 
 main :: IO ()
 main = do
-  defaultMain
-    [ bench "overIndex" $ nf (overIndex 0 (+1)) (toWordArray maxBound)
-    , bench "iforWordArray" $ nf (flip iforWordArray (\i w -> Const $ Sum i)) (toWordArray maxBound)
-    ]
-  putStr "Memory usage :"
-  Weigh.mainWith $ do
-    -- Weigh.action "Word8" w8
-    -- Weigh.action "WordN 8" wn8
-    -- Weigh.action "OddWord 8 Word8" oddword8
-    pure ()
+  let someArray = toWordArray (maxBound `div` 2)
+      {-# NOINLINE someArray #-}
+      mkPrimArray = do
+          arr <- newPrimArray 8
+          setPrimArray arr 0 8 (0 :: Word8)
+          pure arr
+      {-# NOINLINE mkPrimArray #-}
+      overIndexPA :: (Prim a, PrimMonad m) => Int -> (a -> a) -> MutablePrimArray (PrimState m) a -> m ()
+      overIndexPA i f arr = do
+          v <- readPrimArray arr i
+          writePrimArray arr i (f v)
+      iforPrimArray :: (Prim a, PrimMonad m) => MutablePrimArray (PrimState m) a -> (Int -> a -> m ()) -> m ()
+      iforPrimArray arr f = for_ [0..sizeofMutablePrimArray arr] $ \i -> do
+          v <- readPrimArray arr i
+          f i v
 
+  defaultMain
+    [
+      bgroup "word-array"
+        [ bench "overIndex" $ nf (overIndex 0 (+1)) someArray
+        , bench "ifor" $ nf (flip iforWordArray (\i w -> Const $ Sum i)) (toWordArray maxBound)
+        ]
+      , bgroup "prim-array"
+        [ env mkPrimArray $ \arr -> bench "overIndex" $ nfIO (overIndexPA 0 (+1) arr)
+        , env mkPrimArray $ \arr -> bench "ifor" $ nfIO (flip iforPrimArray (\i w -> pure ()) arr)
+        ]
+    ]
