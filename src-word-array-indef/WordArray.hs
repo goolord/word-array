e2f385d51164cc3df2e910fd124668f3a152a300
Zachary Churchill <zacharyachurchill@gmail.com>
2022-08-21 16:57:26 -0400

    synopsis

--- src-word-array-indef/WordArray.hs
+++ src-word-array-indef/WordArray.hs
@@ -59,7 +59,7 @@ displayWordArray wa = displayWordArrayS wa ""
 
 {-# INLINE toTuple #-}
 toTuple :: WordArray -> (# Element WordArray, Element WordArray, Element WordArray, Element WordArray, Element WordArray, Element WordArray, Element WordArray, Element WordArray #)
-toTuple (WordArray !w) = 
+toTuple (WordArray !w) =
   let
     !w7 = w
     !w6 = unsafeShiftR w7 8
@@ -69,7 +69,7 @@ toTuple (WordArray !w) =
     !w2 = unsafeShiftR w3 8
     !w1 = unsafeShiftR w2 8
     !w0 = unsafeShiftR w1 8
-  in 
+  in
   (# fromIntegral w0
   ,  fromIntegral w1
   ,  fromIntegral w2
@@ -87,7 +87,7 @@ toList w =
   in [w0, w1, w2, w3, w4, w5, w6, w7]
 
 index :: WordArray -> Index -> Element WordArray
-index (WordArray w) (Index i) = 
+index (WordArray w) (Index i) =
   let offset = (-8 * i) + 56
   in fromIntegral $ unsafeShiftR w offset
 
@@ -117,19 +117,19 @@ mask _ = error "mask"
 iforWordArray :: Applicative f => WordArray -> (Int -> Element WordArray -> f ()) -> f ()
 iforWordArray w f =
   let (# !w0, !w1, !w2, !w3, !w4, !w5, !w6, !w7 #) = toTuple w
-  in   f 0 (fromIntegral w0) 
-    *> f 1 (fromIntegral w1) 
-    *> f 2 (fromIntegral w2) 
-    *> f 3 (fromIntegral w3) 
-    *> f 4 (fromIntegral w4) 
-    *> f 5 (fromIntegral w5) 
-    *> f 6 (fromIntegral w6) 
+  in   f 0 (fromIntegral w0)
+    *> f 1 (fromIntegral w1)
+    *> f 2 (fromIntegral w2)
+    *> f 3 (fromIntegral w3)
+    *> f 4 (fromIntegral w4)
+    *> f 5 (fromIntegral w5)
+    *> f 6 (fromIntegral w6)
     *> f 7 (fromIntegral w7)
 
 instance MonoFunctor WordArray where
-  omap f w = 
+  omap f w =
     let (# !w0, !w1, !w2, !w3, !w4, !w5, !w6, !w7 #) = toTuple w
-    in WordArray 
+    in WordArray
       (                (fromIntegral (f (fromIntegral w0)))
       .|. unsafeShiftL (fromIntegral (f (fromIntegral w1))) 8
       .|. unsafeShiftL (fromIntegral (f (fromIntegral w2))) 16
@@ -143,27 +143,27 @@ instance MonoFunctor WordArray where
 instance MonoFoldable WordArray where
   ofoldr f b w =
     let (# !w0, !w1, !w2, !w3, !w4, !w5, !w6, !w7 #) = toTuple w
-    in  f (fromIntegral w0) 
-      $ f (fromIntegral w1) 
-      $ f (fromIntegral w2) 
-      $ f (fromIntegral w3) 
-      $ f (fromIntegral w4) 
-      $ f (fromIntegral w5) 
-      $ f (fromIntegral w6) 
+    in  f (fromIntegral w0)
+      $ f (fromIntegral w1)
+      $ f (fromIntegral w2)
+      $ f (fromIntegral w3)
+      $ f (fromIntegral w4)
+      $ f (fromIntegral w5)
+      $ f (fromIntegral w6)
       $ f (fromIntegral w7) b
   ofoldl' f z0 xs = ofoldr f' id xs z0
     where f' x k z = k $! f z x
   ofoldMap f = ofoldr (mappend . f) mempty
   onull _ = False
   oelem e = ofoldr (\a b -> a == e || b) False
-  ofoldr1Ex f xs = fromMaybe 
+  ofoldr1Ex f xs = fromMaybe
       (errorWithoutStackTrace "error in word-array ofoldr1Ex: empty array")
       (ofoldr mf Nothing xs)
     where
     mf x m = Just $ case m of
       Nothing -> x
       Just y  -> f x y
-  ofoldl1Ex' f xs = fromMaybe 
+  ofoldl1Ex' f xs = fromMaybe
       (errorWithoutStackTrace "error in word-array ofoldr1Ex: empty array")
       (ofoldl' mf Nothing xs)
     where
